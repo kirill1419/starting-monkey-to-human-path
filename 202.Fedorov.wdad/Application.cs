1fa869993cb664dbb96798c5224edeace9c13ca9
Porosyatko <valera2020657632578@gmail.com>
2025-02-12 10:11:44 +0300

    Added entry point

--- 202.Fedorov.wdad/Application.cs
+++ 202.Fedorov.wdad/Application.cs
@@ -1,10 +1,10 @@
 ﻿namespace starting_monkey_to_human_path
 {
-    internal class Program
+    internal class Application
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("I’m Kirill Fedorovv, and I’m not a monkey");
         }
     }
 }
